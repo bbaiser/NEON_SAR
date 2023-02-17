9511f0d59b0fc2cad542233e41d72900a99c35e5
bbaiser <benj.baiser@gmail.com>
2023-02-17 15:37:58 -0500

    got paramters for birds, mammals, beetles
    plants have an issue
    ticks seem to be on all plots. is that possible

--- Code/Beetle_SAR.R
+++ Code/Beetle_SAR.R
@@ -1,5 +1,5 @@
 
-
+#load packages
 
 if(!require("neonDivData"))
   install.packages('neonDivData', repos = c(
@@ -12,9 +12,9 @@ library(dplyr)
 install.packages("sars")
 library(sars)
 
-
+####Beetles####
 #get pres/abs for each species per site
-df2 <- data_beetle %>% 
+beetle_df <- data_beetle %>% 
        mutate(present = 1) %>% 
        pivot_wider(names_from = taxon_name, values_from = present) %>% 
       mutate_at(vars(unique(data_beetle$taxon_name)), ~ifelse(is.na(.), 0, 1))%>%
@@ -27,21 +27,21 @@ df2 <- data_beetle %>%
        ungroup()
 
 #run species acc for one site        
-zz<-vegan::specaccum(df2[,-1], method = "exact", subset = df2$siteID=="ABBY")   
+zz<-vegan::specaccum(beetle_df[,-1], method = "exact", subset = beetle_df$siteID=="ABBY")   
 
 #make a list of site names
-varlist <- unique(df2$siteID)
+varlist <- unique(beetle_df$siteID)
 
 #use for loop to loop species accumulation curves over plots in each site (using exact method)
 rich<-list()
 
 for(i in varlist) {                                              
-    acc<-vegan::specaccum(df2[,-1], method = "exact", subset = df2$siteID==i)
+    acc<-vegan::specaccum(beetle_df[,-1], method = "exact", subset = beetle_df$siteID==i)
     rich[[i]]<-acc$richness#extract the richness for each site number
  }
 
 #this is the number of plots at each location
-plot_num<-df2%>%
+plot_num<-beetle_df%>%
           count(siteID)
 
 #just makea vector of plot numbers for loop below
@@ -72,11 +72,11 @@ sp_rich<-unlist(rich[2])
 #for loop the sar_power function from the "sars" package over the site richness area data
 
 #make data frame for for loop
-params <- data.frame(matrix(NA,
+beetle_params <- data.frame(matrix(NA,
                             nrow = 47,
                             ncol = 2),
                             row.names =varlist)
-colnames(params)<-c("c","z")
+colnames(beetle_params)<-c("c","z")
                       
 #run forloop extracting c and z parameters
 for(i in 1:47){
@@ -85,14 +85,14 @@ for(i in 1:47){
               sp_rich<-unlist(rich[i])
               t_dat<-cbind(area,sp_rich)
               fit <- sar_power(data=t_dat)
-              params[i,1]<-fit$par[1]
-              params[i,2]<-fit$par[2]
+              beetle_params[i,1]<-fit$par[1]
+              beetle_params[i,2]<-fit$par[2]
 }
 
-#data frame with parameters
-params
+#data frame with parameters(note that c (intercept) is back transformed)
+beetle_params
 
-####fit multiple models####
+#fit multiple models#
 fitC <- sar_multi(data = t_dat, obj = c("power", "loga", "monod"))
 
 fitC[1]
@@ -100,3 +100,343 @@ fitC[1]
 fit_multi <- sar_average(data = t_dat, grid_start = "none")
 summary(fit_multi)
 plot(fit_multi)
+
+
+
+
+####Birds####
+
+#get pres/abs for each species per site
+df_bird <- data_bird  %>% 
+  mutate(present = 1) %>% 
+  pivot_wider(names_from = taxon_name, values_from = present) %>% 
+  mutate_at(vars(unique(data_bird$taxon_name)), ~ifelse(is.na(.), 0, 1))%>%
+  select(2,3,35:610)%>% 
+  group_by(plotID,siteID)%>% 
+  summarise(across(everything(), list(sum)))%>% 
+  mutate_if(is.numeric, ~1 * (. > 0))%>% 
+  remove_rownames() %>%
+  column_to_rownames(var = 'plotID')%>% 
+  ungroup()
+
+#run species acc for one site        
+zz<-vegan::specaccum(df_bird[,-1], method = "exact", subset = df_bird$siteID=="ABBY")   
+plot(zz)
+#make a list of site names
+varlist <- unique(df_bird$siteID)
+
+#use for loop to loop species accumulation curves over plots in each site (using exact method)
+rich<-list()
+
+for(i in varlist) {                                              
+  acc<-vegan::specaccum(df_bird[,-1], method = "exact", subset = df_bird$siteID==i)
+  rich[[i]]<-acc$richness#extract the richness for each site number
+}
+rich$ABBY
+#this is the number of plots at each location
+plot_num<-df_bird%>%
+         count(siteID)
+
+#just makea vector of plot numbers for loop below
+plotnum<-plot_num$n
+
+
+#make empty matrix to fill with for loop
+matrixe<-matrix(NA, length(plotnum), max(plotnum)+1)
+
+#for loop returns the #40 as many times as there are sampling plots in a site (for loop from Isa)
+for(i in 1:length(plotnum)){
+  
+  obj<-as.vector(plotnum[i])
+  max<-plotnum[i]+1
+  obj[2:max]<-rep(40,plotnum[i])
+  matrixe[i,1:length(obj)]<-obj
+  
+}
+
+#Matrix output
+matrixe
+
+
+#extract area and sprich from one site and run 
+area<-na.omit(cumsum(matrixe[47,-1]))
+sp_rich<-unlist(rich[47])
+
+
+summary(lm(log(sp_rich)~log(area)))
+#for loop the sar_power function from the "sars" package over the site richness area data
+exp( 2.97012 )
+#make data frame for for loop
+bird_params <- data.frame(matrix(NA,
+                            nrow = 47,
+                            ncol = 2),
+                     row.names =varlist)
+
+colnames(bird_params)<-c("c","z")
+
+#run forloop extracting c and z parameters
+for(i in 1:47){
+  
+  area<-na.omit(cumsum(matrixe[i,-1]))
+  sp_rich<-unlist(rich[i])
+  t_dat<-cbind(area,sp_rich)
+  fit <- sar_power(data=t_dat)
+  bird_params[i,1]<-fit$par[1]
+  bird_params[i,2]<-fit$par[2]
+}
+
+#data frame with parameters(note that c (intercept) is back transformed)
+bird_params
+
+####Plants####
+
+#get pres/abs for each species per site
+df_plant <- data_plant  %>% 
+  mutate(present = 1) %>% 
+  pivot_wider(names_from = taxon_name, values_from = present) %>% 
+  mutate_at(vars(unique(data_plant$taxon_name)), ~ifelse(is.na(.), 0, 1))%>%
+  select(2,3,35:610)%>% 
+  group_by(plotID,siteID)%>% 
+  summarise(across(everything(), list(sum)))%>% 
+  mutate_if(is.numeric, ~1 * (. > 0))%>% 
+  remove_rownames() %>%
+  column_to_rownames(var = 'plotID')%>% 
+  ungroup()
+
+#run species acc for one site        
+zz<-vegan::specaccum(df_bird[,-1], method = "exact", subset = df_bird$siteID=="ABBY")   
+plot(zz)
+#make a list of site names
+varlist <- unique(df_bird$siteID)
+
+#use for loop to loop species accumulation curves over plots in each site (using exact method)
+rich<-list()
+
+for(i in varlist) {                                              
+  acc<-vegan::specaccum(df_bird[,-1], method = "exact", subset = df_bird$siteID==i)
+  rich[[i]]<-acc$richness#extract the richness for each site number
+}
+rich$ABBY
+#this is the number of plots at each location
+plot_num<-df_bird%>%
+  count(siteID)
+
+#just makea vector of plot numbers for loop below
+plotnum<-plot_num$n
+
+
+#make empty matrix to fill with for loop
+matrixe<-matrix(NA, length(plotnum), max(plotnum)+1)
+
+#for loop returns the #40 as many times as there are sampling plots in a site (for loop from Isa)
+for(i in 1:length(plotnum)){
+  
+  obj<-as.vector(plotnum[i])
+  max<-plotnum[i]+1
+  obj[2:max]<-rep(40,plotnum[i])
+  matrixe[i,1:length(obj)]<-obj
+  
+}
+
+#Matrix output
+matrixe
+
+
+#extract area and sprich from one site and run 
+area<-na.omit(cumsum(matrixe[47,-1]))
+sp_rich<-unlist(rich[47])
+
+
+summary(lm(log(sp_rich)~log(area)))
+#for loop the sar_power function from the "sars" package over the site richness area data
+exp( 2.97012 )
+#make data frame for for loop
+bird_params <- data.frame(matrix(NA,
+                                 nrow = 47,
+                                 ncol = 2),
+                          row.names =varlist)
+
+colnames(bird_params)<-c("c","z")
+
+#run forloop extracting c and z parameters
+for(i in 1:47){
+  
+  area<-na.omit(cumsum(matrixe[i,-1]))
+  sp_rich<-unlist(rich[i])
+  t_dat<-cbind(area,sp_rich)
+  fit <- sar_power(data=t_dat)
+  bird_params[i,1]<-fit$par[1]
+  bird_params[i,2]<-fit$par[2]
+}
+
+#data frame with parameters(note that c (intercept) is back transformed)
+bird_params
+
+
+####Mammals####
+
+#get pres/abs for each species per site
+df_mammal <- data_small_mammal %>% 
+  mutate(present = 1) %>% 
+  pivot_wider(names_from = taxon_name, values_from = present) %>% 
+  mutate_at(vars(unique(data_small_mammal$taxon_name)), ~ifelse(is.na(.), 0, 1))%>%
+  select(2,3,22:170)%>% 
+  group_by(plotID,siteID)%>% 
+  summarise(across(everything(), list(sum)))%>% 
+  mutate_if(is.numeric, ~1 * (. > 0))%>% 
+  remove_rownames() %>%
+  column_to_rownames(var = 'plotID')%>% 
+  ungroup()
+
+#run species acc for one site        
+zz<-vegan::specaccum(df_mammal[,-1], method = "exact", subset = df_mammal$siteID=="ABBY")   
+plot(zz)
+#make a list of site names
+varlist <- unique(df_mammal$siteID)
+
+#use for loop to loop species accumulation curves over plots in each site (using exact method)
+rich<-list()
+
+for(i in varlist) {                                              
+  acc<-vegan::specaccum(df_mammal[,-1], method = "exact", subset = df_mammal$siteID==i)
+  rich[[i]]<-acc$richness#extract the richness for each site number
+}
+rich$ABBY
+#this is the number of plots at each location
+plot_num<-df_mammal%>%
+  count(siteID)
+
+#just makea vector of plot numbers for loop below
+plotnum<-plot_num$n
+
+
+#make empty matrix to fill with for loop
+matrixe<-matrix(NA, length(plotnum), max(plotnum)+1)
+
+#for loop returns the #40 as many times as there are sampling plots in a site (for loop from Isa)
+for(i in 1:length(plotnum)){
+  
+  obj<-as.vector(plotnum[i])
+  max<-plotnum[i]+1
+  obj[2:max]<-rep(40,plotnum[i])
+  matrixe[i,1:length(obj)]<-obj
+  
+}
+
+#Matrix output
+matrixe
+
+
+#extract area and sprich from one site and run 
+area<-na.omit(cumsum(matrixe[46,-1]))
+sp_rich<-unlist(rich[46])
+summary(lm(log(sp_rich)~log(area)))
+
+#for loop the sar_power function from the "sars" package over the site richness area data
+
+#make data frame for for loop
+mammal_params <- data.frame(matrix(NA,
+                                 nrow = 46,
+                                 ncol = 2),
+                          row.names =varlist)
+
+colnames(mammal_params)<-c("c","z")
+
+#run forloop extracting c and z parameters
+for(i in 1:46){
+  
+  area<-na.omit(cumsum(matrixe[i,-1]))
+  sp_rich<-unlist(rich[i])
+  t_dat<-cbind(area,sp_rich)
+  fit <- sar_power(data=t_dat)
+  mammal_params[i,1]<-fit$par[1]
+  mammal_params[i,2]<-fit$par[2]
+}
+
+#data frame with parameters(note that c (intercept) is back transformed)
+mammal_params
+
+####Ticks#### 
+looks lime all plots have all ticks?
+colnames(data_bird)
+#get pres/abs for each species per site
+df_tick <- data_tick %>% 
+  mutate(present = 1) %>% 
+  pivot_wider(names_from = taxon_name, values_from = present) %>% 
+  mutate_at(vars(unique(data_tick$taxon_name)), ~ifelse(is.na(.), 0, 1))%>%
+  select(2,3,22:40)%>% 
+  group_by(plotID,siteID)%>% 
+  summarise(across(everything(), list(sum)))%>% 
+  mutate_if(is.numeric, ~1 * (. > 0))%>% 
+  remove_rownames() %>%
+  column_to_rownames(var = 'plotID')%>% 
+  ungroup()
+df_tick$plotID
+
+#run species acc for one site        
+zz<-vegan::specaccum(df_tick[,-1], method = "random", subset = df_tick$siteID=="ABBY")   
+plot(zz)
+#make a list of site names
+varlist <- unique(df_mammal$siteID)
+
+#use for loop to loop species accumulation curves over plots in each site (using exact method)
+rich<-list()
+
+for(i in varlist) {                                              
+  acc<-vegan::specaccum(df_tick[,-1], method = "exact", subset = df_tick$siteID==i)
+  rich[[i]]<-acc$richness#extract the richness for each site number
+}
+rich$ABBY
+#this is the number of plots at each location
+plot_num<-df_mammal%>%
+  count(siteID)
+
+#just makea vector of plot numbers for loop below
+plotnum<-plot_num$n
+
+
+#make empty matrix to fill with for loop
+matrixe<-matrix(NA, length(plotnum), max(plotnum)+1)
+
+#for loop returns the #40 as many times as there are sampling plots in a site (for loop from Isa)
+for(i in 1:length(plotnum)){
+  
+  obj<-as.vector(plotnum[i])
+  max<-plotnum[i]+1
+  obj[2:max]<-rep(40,plotnum[i])
+  matrixe[i,1:length(obj)]<-obj
+  
+}
+
+#Matrix output
+matrixe
+
+
+#extract area and sprich from one site and run 
+area<-na.omit(cumsum(matrixe[46,-1]))
+sp_rich<-unlist(rich[46])
+summary(lm(log(sp_rich)~log(area)))
+
+#for loop the sar_power function from the "sars" package over the site richness area data
+
+#make data frame for for loop
+tick_params <- data.frame(matrix(NA,
+                                   nrow = 46,
+                                   ncol = 2),
+                            row.names =varlist)
+
+colnames(tick_params)<-c("c","z")
+
+#run forloop extracting c and z parameters
+for(i in 1:46){
+  
+  area<-na.omit(cumsum(matrixe[i,-1]))
+  sp_rich<-unlist(rich[i])
+  t_dat<-cbind(area,sp_rich)
+  fit <- sar_power(data=t_dat)
+  tick_params[i,1]<-fit$par[1]
+  tick_params[i,2]<-fit$par[2]
+}
+
+#data frame with parameters(note that c (intercept) is back transformed)
+tick_params
