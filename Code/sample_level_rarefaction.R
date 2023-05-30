d10ad66c8fe6961b2c5c98c0aaeb58edecca5fdf
bbaiser <benj.baiser@gmail.com>
2023-05-30 09:34:07 -0400

    cleaned up code

--- Code/sample_level_rarefaction.R
+++ Code/sample_level_rarefaction.R
@@ -1,51 +1,42 @@
+#Rarefaction/Extrapolation using iNEXT for NEON sites
+
 ####Beetles####
 
-#obtain a site by species data frame th
+#obtain a sample by species data frame where each row is a different sampling bout
 beetle_df <- data_beetle %>% 
-             select(siteID, plotID, taxon_name,boutID) %>% 
+             select(siteID, plotID, taxon_name,unique_sample_id) %>% 
+             rename(sample=unique_sample_id)%>% 
              mutate(present = 1) %>% 
-             mutate(sample = paste(plotID, boutID, sep = "_")) %>% 
-             group_by(sample, taxon_name, siteID)%>% 
+             group_by(sample,siteID,taxon_name)%>%
              summarise(present = sum(present)/sum(present)) %>% 
              pivot_wider(names_from = taxon_name, values_from = present, values_fill = 0) %>%
              ungroup()%>% 
              select(-c(sample))
 
-#make a list of matrices (site-by-species) to run in iNEXT
-lm<-split(beetle_df,beetle_df$siteID)%>%
+#make a list of matrices (sample-by-species) to run in iNEXT
+list_beetles<-split(beetle_df,beetle_df$siteID)%>%
     lapply(., function(x)x[,-1 ])%>%
     lapply(.,t)
-ad<-lm$ABBY
 
-#Run iNEXT for species richness q=0)
-out.raw <- iNEXT(lm, q = 0, datatype="incidence_raw")
 
-#SUBSET INEXT OUTPUT FOR SPECIES RICHNESS ONLY
-ex_rich<-out.raw$AsyEst%>%
-         filter(Diversity=="Species richness")%>%
-         rename(siteID=Assemblage)
+#Run iNEXT for species richness (q=0)
+out_raw<- iNEXT(list_beetles, q = 0, datatype="incidence_raw")
 
-#Try and select sites where observed is in extrapolated ci's
 
-beetle_rar<-out.raw$iNextEst$size_based%>%
+#Calculate the percent of the extrapolated richness reached by each sample 
+#and if the observed estimate falls in the 95% CIs of the extrapolation
+
+beetle_rar<-out_raw_beet $iNextEst$size_based%>%
              rename(siteID=Assemblage)%>%
              group_by(siteID)%>%
-             slice(20,40)%>%#take the observed richness line and the final exatrapolated line
-             mutate(fit= qD>=qD.LCL[row_number()+1] & qD<=qD.UCL[row_number()+1])%>%#compare the onserved to the lcl and ucl of the exatrapolation
+             slice(20,40)%>%#take the observed richness line and the final extrapolated line
+             mutate(fit= qD>=qD.LCL[row_number()+1] & qD<=qD.UCL[row_number()+1])%>%#compare the observed to the lcl and ucl of the extrapolation
              mutate(percent= qD/qD[row_number()+1])%>%
              filter(Method=="Observed")%>%
              select(siteID,qD,fit, percent)
 
 write.csv(beetle_rar,"./data/beetle_rar.csv")
 
-
-
-  
-  
-
-#rarefy down to the lowest species richness with estimate D
-d<-estimateD(lm, datatype="incidence_raw",q = 0, base="size", level=7)%>%
-   rename(siteID=Assemblage)
 
 
 #do sites with more samples have greater extrapolated richness estimates?
@@ -58,7 +49,7 @@ site_data<-read.csv("Data/NEON_Field_Site_Metadata_20220412.csv")%>%
 #get beetle SAR parameters
 beetle_params<-read.csv("Data/beetle_params.csv")
 
-# get beetle sampling covariates
+#get beetle sampling covariates
 beetle_vars<-read.csv("Data/beetle_vars.csv",row=1)
 
 #get inter plot distances
@@ -66,8 +57,6 @@ beetle_dist<-read.csv("Data/organismalPlotMeanDist.csv",row=1)%>%
              filter(taxon=="beetle")%>%
              rename(siteID = site)%>%
              select(siteID,aveDist)
-
-
 
 #combine into one dataframe with other beetle data
 comb_beetle<-beetle_params%>%
@@ -77,8 +66,8 @@ comb_beetle<-beetle_params%>%
             left_join(beetle_dist,by="siteID")%>%
             left_join(beetle_rar,by="siteID")%>%
             subset(.,siteID!="GUAN"& siteID!="PUUM"& siteID!="LAJA")%>%#remove puerto rico and Hawaii sites and STER
-            filter(fit=="TRUE")         
-          # filter(percent>=.90)#to filter out sites with less than 40 obs
+            #filter(fit=="TRUE")         
+            filter(percent>=.80)#to filter out sites with less than 40 obs
 
 ##LOOK AT RELATIONSHIPS
 mod<-lm(comb_beetle$qD~comb_beetle$n_observation)
@@ -87,35 +76,31 @@ plot(comb_beetle$n_observation,comb_beetle$qD)
 
 ####Small_mammals####
 
-#obtain a site by species data frame th
+#obtain a sample by species data frame where each row is a different sampling bout
 mammal_df <- data_small_mammal %>% 
               select(siteID, plotID, taxon_name,unique_sample_id) %>% 
               mutate(present = 1) %>% 
-              #mutate(sample = paste(plotID, boutID, sep = "_")) %>% 
               rename(sample=unique_sample_id)%>% 
               group_by(sample, taxon_name, siteID)%>% 
               summarise(present = sum(present)/sum(present)) %>% 
               pivot_wider(names_from = taxon_name, values_from = present, values_fill = 0) %>%
-              subset(.,siteID!="GUAN"& siteID!="BARR"& siteID!="LAJA")%>%
+              subset(.,siteID!="GUAN"& siteID!="BARR"& siteID!="LAJA")%>%#remove these cause they have too few samples for iNEXT
               ungroup()%>% 
               select(-c(sample))
 
-#make a list of matrices (site-by-species) to run in iNEXT
-lm<-split(mammal_df,mammal_df$siteID)%>%
-   lapply(., function(x)x[,-1 ])%>%
-   lapply(.,t)
+#make a list of matrices (sample-by-species) to run in iNEXT
+list_mam<-split(mammal_df,mammal_df$siteID)%>%
+    lapply(., function(x)x[,-1 ])%>%
+    lapply(.,t)
 
 
 #Run iNEXT for species richness q=0)
-out.raw_mam <- iNEXT(lm, q = 0, datatype="incidence_raw")
+out.raw_mam <- iNEXT(list_mam, q = 0, datatype="incidence_raw")
 
-#SUBSET INEXT OUTPUT FOR SPECIES RICHNESS ONLY
-ex_rich<-out.raw_mam$AsyEst%>%
-         filter(Diversity=="Species richness")%>%
-         rename(siteID=Assemblage)
 
-#Try and select sites where observed is in extrapolated ci's
 
+#Calculate the percent of the extrapolated richness reached by each sample 
+#and if the observed estimate falls in the 95% CIs of the extrapolation
 mammal_rar<-out.raw_mam$iNextEst$size_based%>%
             rename(siteID=Assemblage)%>%
             group_by(siteID)%>%
@@ -127,14 +112,6 @@ mammal_rar<-out.raw_mam$iNextEst$size_based%>%
 
 write.csv(mammal_rar,"./data/mammal_rar.csv")
 
-
-
-
-
-
-#rarefy down to the lowest species richness with estimate D
-d<-estimateD(lm, datatype="incidence_raw",q = 0, base="size", level=7)%>%
-  rename(siteID=Assemblage)
 
 
 #do sites with more samples have greater extrapolated richness estimates?
@@ -175,37 +152,32 @@ summary(mod)
 plot(comb_mammal$n_observation,comb_mammal$qD)
 
 
-####birds####
-#obtain a site by species data frame th
+####Birds####
+#obtain a sample by species data frame where each row is a different sampling bout
 bird_df <- data_bird %>% 
           select(siteID, plotID, taxon_name,unique_sample_id) %>% 
           mutate(present = 1) %>% 
-          #mutate(sample = paste(plotID, boutID, sep = "_")) %>% 
           rename(sample=unique_sample_id)%>% 
           group_by(sample, taxon_name, siteID)%>% 
           summarise(present = sum(present)/sum(present)) %>% 
           pivot_wider(names_from = taxon_name, values_from = present, values_fill = 0) %>%
-          #subset(.,siteID!="GUAN"& siteID!="BARR"& siteID!="LAJA")%>%
           ungroup()%>% 
           select(-c(sample))
 
 #make a list of matrices (site-by-species) to run in iNEXT
-lm<-split(bird_df,bird_df$siteID)%>%
-    lapply(., function(x)x[,-1 ])%>%
-    lapply(.,t)
+list_bird<-split(bird_df,bird_df$siteID)%>%
+            lapply(., function(x)x[,-1 ])%>%
+            lapply(.,t)
 
 
 #Run iNEXT for species richness q=0)
-out.raw_mam <- iNEXT(lm, q = 0, datatype="incidence_raw")
+out.raw_bird <- iNEXT(list_bird, q = 0, datatype="incidence_raw")
 
-#SUBSET INEXT OUTPUT FOR SPECIES RICHNESS ONLY
-ex_rich<-out.raw_mam$AsyEst%>%
-        filter(Diversity=="Species richness")%>%
-        rename(siteID=Assemblage)
 
-#Try and select sites where observed is in extrapolated ci's
 
-bird_rar<-out.raw_mam$iNextEst$size_based%>%
+#Calculate the percent of the extrapolated richness reached by each sample 
+#and if the observed estimate falls in the 95% CIs of the extrapolation
+bird_rar<-out.raw_bird$iNextEst$size_based%>%
           rename(siteID=Assemblage)%>%
           group_by(siteID)%>%
           slice(20,40)%>%#take the observed richness line and the final exatrapolated line
@@ -216,14 +188,6 @@ bird_rar<-out.raw_mam$iNextEst$size_based%>%
 
 write.csv(bird_rar,"./data/bird_rar.csv")
 
-
-
-
-
-
-#rarefy down to the lowest species richness with estimate D
-d<-estimateD(lm, datatype="incidence_raw",q = 0, base="size", level=7)%>%
-  rename(siteID=Assemblage)
 
 
 #do sites with more samples have greater extrapolated richness estimates?
@@ -241,9 +205,9 @@ bird_vars<-read.csv("Data/bird_vars.csv",row=1)
 
 #get inter plot distances
 bird_dist<-read.csv("Data/organismalPlotMeanDist.csv",row=1)%>%
-  filter(taxon=="bird")%>%
-  rename(siteID = site)%>%
-  select(siteID,aveDist)
+          filter(taxon=="bird")%>%
+          rename(siteID = site)%>%
+          select(siteID,aveDist)
 
 
 
@@ -255,7 +219,7 @@ comb_bird<-bird_params%>%
           left_join(bird_dist,by="siteID")%>%
           left_join(bird_rar,by="siteID")%>%
           subset(.,siteID!="GUAN"& siteID!="PUUM"& siteID!="LAJA")%>%#remove puerto rico and Hawaii sites and STER
-          #filter(fit=="TRUE")         
+          filter(fit=="TRUE")         
           filter(percent>=.9)#to filter out sites with less than 40 obs
 
 ##LOOK AT RELATIONSHIPS
@@ -264,35 +228,30 @@ summary(mod)
 plot(comb_bird$n_observation,comb_bird$qD)
 
 ####plants####
-#obtain a site by species data frame th
+#obtain a sample by species data frame where each row is a different sampling bout
 plant_df <- data_plant %>% 
           select(siteID, plotID, taxon_name,unique_sample_id) %>% 
           mutate(present = 1) %>% 
-          #mutate(sample = paste(plotID, boutID, sep = "_")) %>% 
           rename(sample=unique_sample_id)%>% 
           group_by(sample, taxon_name, siteID)%>% 
           summarise(present = sum(present)/sum(present)) %>% 
           pivot_wider(names_from = taxon_name, values_from = present, values_fill = 0) %>%
-          #subset(.,siteID!="GUAN"& siteID!="BARR"& siteID!="LAJA")%>%
           ungroup()%>% 
           select(-c(sample))
 
-#make a list of matrices (site-by-species) to run in iNEXT
-lm<-split(plant_df,plant_df$siteID)%>%
+#make a list of matrices (sample-by-species) to run in iNEXT
+list_plant<-split(plant_df,plant_df$siteID)%>%
     lapply(., function(x)x[,-1 ])%>%
     lapply(.,t)
 
 
 #Run iNEXT for species richness q=0)
-out.raw_plant <- iNEXT(lm, q = 0, datatype="incidence_raw")
+out.raw_plant <- iNEXT(list_plant, q = 0, datatype="incidence_raw")
 
-#SUBSET INEXT OUTPUT FOR SPECIES RICHNESS ONLY
-ex_rich<-out.raw_plant$AsyEst%>%
-         filter(Diversity=="Species richness")%>%
-         rename(siteID=Assemblage)
 
-#Try and select sites where observed is in extrapolated ci's
 
+#Calculate the percent of the extrapolated richness reached by each sample 
+#and if the observed estimate falls in the 95% CIs of the extrapolation
 plant_rar<-out.raw_plant$iNextEst$size_based%>%
           rename(siteID=Assemblage)%>%
           group_by(siteID)%>%
@@ -306,12 +265,6 @@ write.csv(plant_rar,"./data/plant_rar.csv")
 
 
 
-
-
-
-#rarefy down to the lowest species richness with estimate D
-d<-estimateD(lm, datatype="incidence_raw",q = 0, base="size", level=7)%>%
-  rename(siteID=Assemblage)
 
 
 #do sites with more samples have greater extrapolated richness estimates?
