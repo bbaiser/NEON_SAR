92405c1b0869fe1d2c9533d289367aaa5895ec23
bbaiser <benj.baiser@gmail.com>
2023-11-06 21:32:41 -0500

    need to change from numeric to character

--- Code/plot_rarefaction.R
+++ Code/plot_rarefaction.R
@@ -17,26 +17,47 @@ library(ggforce)
 
 ####Beetles####
 
-#obtain a site by species data frame for all sites
-beetle_df <- data_beetle %>% 
-  unite('sample',plotID, boutID, remove=F )%>%
-  select(sample ,plotID, taxon_name) %>% 
-  mutate(present = 1) %>% 
-  group_by(sample,plotID, taxon_name) %>% 
-  summarise(present = sum(present)) %>% 
-  pivot_wider(names_from = taxon_name, values_from = present)%>%  
-  remove_rownames() %>%
-  column_to_rownames(var = 'sample')    
+#obtain species frequencies per plot across sampling bouts
+beetle_df <-  data_beetle %>% 
+              select(plotID, taxon_name) %>% 
+              mutate(present = 1) %>% 
+              group_by(plotID, taxon_name) %>%
+              summarise(present = sum(present)) %>%
+              summarize(present = paste((present),collapse=", "))
+
+
+  #obtain number of samples per plot
+ n_samp <-  data_beetle %>% 
+            unite('sample',plotID, boutID, remove=F )%>%
+            select(sample, plotID) %>%
+            unique()%>%
+            group_by(plotID) %>% 
+            summarise(n = n()) 
  
 
+ 
+ rar_in<-beetle_df %>%
+        left_join(n_samp, by="plotID")%>%
+        unite('sample',n, present, sep= ",")%>%
+        column_to_rownames(var = 'plotID')  %>% 
+        mutate_if(is.character,as.numeric)
 
+ z<-(rar_in[1,])
+x<-as.vector(z)
+as.numeric(as.list(x) )
+ as.numeric(rar_in)
+data1 <- as.data.frame(rar_in)%>%
+         as.numeric(unlist(.))
+data_list <- split(data1,data1$plotID)
+
+data_list[["1"]]
 #make a list of matrices (site-by-species) to run in iNEXT
-lm<-split(beetle_df,beetle_df$plotID)%>%
+lm<-split(rar_in,rar_in$plotID)%>%
   lapply(., function(x)x[,-1 ])%>%
   lapply(.,t)
-lm$ABBY_002
+
 #Run iNEXT for species richness q=0)
-out.raw <- iNEXT(lm, q = 0, datatype="incidence_freq")
+out.raw <- iNEXT(rar_in, q = 0, datatype="incidence_freq")
 
 #SUBSET FOR SPECIES RICHNESS ONLY
 ex<-out.raw$AsyEst%>%
